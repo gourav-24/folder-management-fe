3a4842801e5e450fdbe2b5f9e6d6fd9f744a4599
Gourav Deswal <gouravdeswal@Gouravs-MacBook-Air.local>
2024-12-23 22:01:01 +0530

    route changes

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -1,12 +1,3 @@
-// // app/api/menu/route.ts
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function GET(req: NextRequest) {
-//   const menuData = {
-//     id: "56320ee9-6af6-11ed-a7ba-f220afe5e4a9",
-//     name: "System Code",
-//     depth: 3,
-//     parent: "Systems",
-//   };
-//   return NextResponse.json(menuData);
-// }
+export default function handler() {
+  return new Response('Not implemented', { status: 501 });
+}
